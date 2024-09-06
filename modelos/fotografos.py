503991e89a8c7409b9bb414b039afb5355b1a6af
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-06 10:24:58 -0300

    Add files via upload

---
+++ modelos/fotografos.py
@@ -0,0 +1,12 @@
+class Fotografo:
+    nome = ''
+    categoria = ''
+    agenda  = ''
+    ativo = False
+
+fotografo_Igor = Fotografo()
+fotografo_Sofia = Fotografo()
+
+fotografos = [fotografo_Igor, fotografo_Sofia]
+
+print(fotografos)
