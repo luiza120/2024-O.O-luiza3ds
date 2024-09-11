e789d85d20aff0665e88d46c8fcf6f2972d5509e
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-11 11:55:02 -0300

    att

---
+++ 2024-py-O.O-luiza3ds/modelos/fotografos.py
@@ -0,0 +1,17 @@
+class Fotografo:
+    nome = ''
+    categoria = ''
+    agenda  = ''
+    ativo = False
+
+fotografo_Igor = Fotografo()
+fotografo_Sofia = Fotografo()
+fotografo_Natalia = Fotografo()
+
+fotografo_Sofia.nome = 'Sofia Starling'
+fotografo_Sofia.categoria = 'Retratista'
+
+fotografos = [fotografo_Igor, fotografo_Sofia, fotografo_Natalia]
+
+print(fotografos/n)
+print(fotografo_Sofia/n)
