15e7de5f69275f15f960df83fe3df6c0061be65a
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-12 12:09:59 -0300

    att

--- modelos/fotografos.py
+++ modelos/fotografos.py
@@ -1,17 +1,18 @@
 class Fotografo:
-    nome = ''
-    categoria = ''
-    agenda  = ''
-    ativo = False
+    def __init__(self, nome, categoria, agenda, ativo):
+        self.nome = nome
+        self.categoria = categoria
+        self.agenda  = agenda
+        self.ativo = ativo
 
-fotografo_Igor = Fotografo()
-fotografo_Sofia = Fotografo()
-fotografo_Natalia = Fotografo()
+    def __str__(self):
+        return f'{self.nome} | {self.categoria} | {self.agenda} | {self.ativo}'
 
-fotografo_Sofia.nome = 'Sofia Starling'
-fotografo_Sofia.categoria = 'Retratista'
+fotografo_Igor = Fotografo('Igor', 'Retratista', 'Seg e Sextas')
+fotografo_Sofia = Fotografo('Sofia', 'Fotografia Social', 'Ter e Sab')
+fotografo_Natalia = Fotografo('Natalia', 'Street Style', 'Qua e Qui')
 
 fotografos = [fotografo_Igor, fotografo_Sofia, fotografo_Natalia]
 
-print(fotografos/n)
-print(fotografo_Sofia/n)
+print(fotografo_Natalia)
+
