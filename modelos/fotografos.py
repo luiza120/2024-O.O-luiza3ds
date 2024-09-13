cb44a852b8c4a49f0b60fd688b53a4a833a1fbd2
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-13 10:40:22 -0300

    att

--- modelos/fotografos.py
+++ modelos/fotografos.py
@@ -6,15 +6,27 @@ class Fotografo:
         self.nome = nome
         self.categoria = categoria
         self.agenda  = agenda
-        self.ativo = False
+        self._ativo = False
         Fotografo.fotografos.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria} | {self.agenda} | {self.ativo}'
 
     def listar_fotografos():
+
+        print('𝓕𝓸𝓽𝓸́𝓰𝓻𝓪𝓯𝓸𝓼')
+        print('')
+        print(f'{"Nome dos fotógrafos".ljust(20)} | {"Categoria".ljust(20)} | {"Agenda".ljust(20)} | {"Ativo"}')
+        print('‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒')
+
         for fotografo in Fotografo.fotografos:
-            print(f'{fotografo.nome} | {fotografo.categoria} | {fotografo.agenda} | {fotografo.ativo}')
+            print(f'{fotografo.nome.ljust(20)} | {fotografo.categoria.ljust(20)} | {fotografo.agenda.ljust(20)} | {fotografo.ativo}')
+            print('‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒')
+
+
+    @property
+    def ativo(self):
+        return '⋆ Ativo(a)' if self._ativo else '≛ Não ativo(a)'
 
 fotografo_Igor = Fotografo('Igor', 'Retratista', 'Seg e Sextas')
 fotografo_Sofia = Fotografo('Sofia', 'Fotografia Social', 'Ter e Sab')
