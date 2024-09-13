4a725275a046a32a7be7eea3a2255cdfff9aa03c
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-13 07:46:13 -0300

    att

--- modelos/fotografos.py
+++ modelos/fotografos.py
@@ -1,18 +1,25 @@
 class Fotografo:
-    def __init__(self, nome, categoria, agenda, ativo):
+    
+    fotografos = []
+    
+    def __init__(self, nome, categoria, agenda):
         self.nome = nome
         self.categoria = categoria
         self.agenda  = agenda
-        self.ativo = ativo
+        self.ativo = False
+        Fotografo.fotografos.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria} | {self.agenda} | {self.ativo}'
+
+    def listar_fotografos():
+        for fotografo in Fotografo.fotografos:
+            print(f'{fotografo.nome} | {fotografo.categoria} | {fotografo.agenda} | {fotografo.ativo}')
 
 fotografo_Igor = Fotografo('Igor', 'Retratista', 'Seg e Sextas')
 fotografo_Sofia = Fotografo('Sofia', 'Fotografia Social', 'Ter e Sab')
 fotografo_Natalia = Fotografo('Natalia', 'Street Style', 'Qua e Qui')
 
-fotografos = [fotografo_Igor, fotografo_Sofia, fotografo_Natalia]
+Fotografo.listar_fotografos()
 
-print(fotografo_Natalia)
 
