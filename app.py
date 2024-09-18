98455ff99c875b4f563693375089fff0c05896f6
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-09-18 11:27:44 -0300

    att

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ fotografo_Natalia = Fotografo('Natália', 'Street Style', 'Qua e Qui')
 fotografo_Caroline = Fotografo('Caroline', 'Publicidade', 'Seg a Sex' )
 
 fotografo_Sofia.alterar_estado()
-fotografo_Igor.receber_eventos('Exposições', 1, 'maio')
-fotografo_Natalia.receber_eventos('Formaturas', 4, 'abril' )
-fotografo_Caroline.receber_eventos('Books', 5, 'junho')
+fotografo_Igor.receber_eventos('Exposições', 8, 'maio')
+fotografo_Natalia.receber_eventos('Ensaio', 4, 'abril' )
+fotografo_Caroline.receber_eventos('Propaganda', 5, 'junho')
 
 
 def main():
